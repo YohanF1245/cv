7546ad0e0934e6373249ffa5eaaf0e58201b2341
Yohan Ferdin <yohan.ferdin@hotmail.fr>
2023-11-20 00:32:14 +0100

    added the popin and pop out function

--- script/realisations.js
+++ script/realisations.js
@@ -250,3 +250,13 @@ function recalculateDmg(){
 	}
 	document.getElementById("DamagePerClick").innerHTML = "Damage per click:"+damage;
 }
+function showCrazyFood(){
+	document.getElementById("crazyFood").classList.remove("modal");
+	document.getElementById("crazyFood").classList.remove("out");
+	document.getElementById("crazyFood").classList.add("in");
+}
+function hideCrazyFood(){
+	document.getElementById("crazyFood").classList.add("in");
+		document.getElementById("crazyFood").classList.add("out");
+		document.getElementById("crazyFood").classList.add("modal");
+}
