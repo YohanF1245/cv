0ee923cfcbbe7ce1ed3c140e6b6d23bdef23f112
Yohan Ferdin <yohan.ferdin@hotmail.fr>
2023-11-25 21:59:30 +0100

    changed keyboard layout for find word

--- script/test.js
+++ script/test.js
@@ -1,9 +1,7 @@
-var listAnimStep = document.getElementsByClassName("animSteps");
+const listAnimStep = document.getElementsByClassName("animSteps");
 var iteration = 0;
 var interval = setInterval(function () {
     listAnimStep[iteration].style.display = "block";
-   iteration++;
-   if(iteration === 8){
-    clearInterval(interval);
-   }
+    iteration++;
+    if(iteration === 8){ clearInterval(interval);}
 }, 500);
