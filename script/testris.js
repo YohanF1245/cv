c3fd0c8aa8953a5508461a1674e0952b374288d6
Yohan F <yohan.ferdin@hotmail.fr>
2023-11-29 16:30:31 +0100

    push of the day

--- script/testris.js
+++ script/testris.js
@@ -3,89 +3,123 @@ var x = 0;
 var hitboxes = [380, 380, 380, 380, 380, 380, 380, 380, 380, 380];
 var canva = document.getElementById("frameApp");
 var ctx = canva.getContext("2d");
-var playMatrix;
-var tertrominos = ["i", "j", "o", "s", "z", "l", "t"];
-var topDraw = [0,0,0];
-var tetraJ = [
-	[0, 1, 0],
-	[0, 1, 0],
-	[1, 1, 0],
+var playMatrix = new Array(20).fill(Array(10).fill(0));
+console.log(playMatrix);
+var topDraw = [0, 0, 0];
+var laBarre = [
+	[0, 1, 0, 0],
+	[0, 1, 0, 0],
+    [0, 1, 0, 0],
+    [0, 1, 0, 0]
 ];
+var z = [
+	[0, 1, 1],
+	[1, 1, 0],
+	[0, 0, 0]
+];
+var s = [
+	[1, 1, 0],
+	[0, 1, 1],
+	[0, 0, 0]
+];
+var t = [
+	[0, 1, 0],
+	[1, 1, 1],
+	[0, 0, 0]
+];
+var o = [
+	[0, 1, 1],
+	[0, 1, 1],
+	[0, 0, 0]
+];
+var l = [
+	[0, 1, 0],
+	[0, 1, 0],
+	[0, 1, 1]
+];
+var ji = [
+	[0, 1, 0],
+	[0, 1, 0],
+	[1, 1, 0]
+];
+var tetraminos = [laBarre, ji, o, s, z, l, t];
+var playerTetra;
+console.log(tetraminos);
+randTetra;
+function randTetra() {
+    var tempT = Math.floor(Math.random() * 6);
+    playerTetra = tetraminos[tempT];
+}
 function kekw() {
 	if (canva.getContext) {
 		document.getElementById("navigateurSupported").innerHTML = "supoprtey";
 	}
 	ctx.fillStyle = "rgb(200, 0, 0)";
-    for (var i = 0; i < 3; i++) {
-        for (var j = 0; j < 3; j++) {
-                 if (tetraJ[j][k] === 1){
-                    if(topDraw[j] === 0){
-                        topDraw[j]=i;
-                    }
-                 }
-		}
-    }
+	console.log("*---------------------------*");
 	for (var i = 0; i < 3; i++) {
-        for (var j = 0; j < 3; j++) {
-                ctx.clearRect(x+(20*i), y+(20*j), 20, 20);
-		}
-    } y++;
-    for (var i = 0; i < 3; i++) {
-        for (var j = 0; j < 3; j++) {
-            if (tetraJ[j][i] === 1) {
-			
-			ctx.fillRect(x+(20*i), y+(20*j), 20, 20);
-			if (y === hitboxes[Math.floor(x / 20)]) {
-				hitboxes[Math.floor(x / 20)] = hitboxes[Math.floor(x / 20)] - 20;
-				y = 0;
+		for (var j = 0; j < 3; j++) {
+			console.log(playerTetra[i][j]);
+			if (playerTetra[i][j] === 1) {
+				if (topDraw[j] === 0) {
+					topDraw[j] = i;
+				}
 			}
-            }
-			
 		}
-    } 
-    
+	}
+	console.log(topDraw);
+	for (var i = 0; i < 3; i++) {
+		for (var j = 0; j < 3; j++) {
+			if (playerTetra[i][j] === 1) {
+				ctx.clearRect(x + 20 * j, y + 20 * i, 20, 20);
+			}
+		}
+	}
+	y++;
+	for (var i = 0; i < 3; i++) {
+		for (var j = 0; j < 3; j++) {
+			if (playerTetra[i][j] === 1) {
+				ctx.fillRect(x + 20 * j, y + 20 * i, 20, 20);
+				if (y === hitboxes[Math.floor(x / 20)]) {
+					hitboxes[Math.floor(x / 20)] = hitboxes[Math.floor(x / 20)] - 20;
+					y = 0;
+				}
+			}
+		}
+	}
 }
 function moveRight() {
 	if (x < 180) {
-        for (var i = 0; i < 3; i++) {
-            for (var j = 0; j < 3; j++) {
-                    ctx.clearRect(x+(20*i), y+(20*j), 20, 20);
-                
-                
-                
-            }
-        }		x += 20;
-        for (var i = 0; i < 3; i++) {
-            for (var j = 0; j < 3; j++) {
-                if (tetraJ[j][i] === 1) {
-                
-                ctx.fillRect(x+(20*i), y+(20*j), 20, 20);
-                
-                }
-                
-            }
-        } 	}
+		for (var i = 0; i < 3; i++) {
+			for (var j = 0; j < 3; j++) {
+				ctx.clearRect(x + 20 * i, y + 20 * j, 20, 20);
+			}
+		}
+		x += 20;
+		for (var i = 0; i < 3; i++) {
+			for (var j = 0; j < 3; j++) {
+				if (playerTetra[i][j] === 1) {
+					ctx.fillRect(x + 20 * j, y + 20 * i, 20, 20);
+				}
+			}
+		}
+	}
 }
 function moveLeft() {
 	if (x < 180) {
-        for (var i = 0; i < 3; i++) {
-            for (var j = 0; j < 3; j++) {
-                    ctx.clearRect(x+(20*i), y+(20*j), 20, 20);
-                
-                
-                
-            }
-        }		x -= 20;
-        for (var i = 0; i < 3; i++) {
-            for (var j = 0; j < 3; j++) {
-                if (tetraJ[j][i] === 1) {
-                
-                ctx.fillRect(x+(20*i), y+(20*j), 20, 20);
-                
-                }
-                
-            }
-        } 	}
+		for (var i = 0; i < 3; i++) {
+			for (var j = 0; j < 3; j++) {
+				ctx.clearRect(x + 20 * i, y + 20 * j, 20, 20);
+			}
+		}
+		x -= 20;
+		for (var i = 0; i < 3; i++) {
+			for (var j = 0; j < 3; j++) {
+				if (playerTetra[j][i] === 1) {
+					ctx.fillRect(x + 20 * i, y + 20 * j, 20, 20);
+				}
+			}
+		}
+	}
 }
 setInterval(kekw, 16);
 addEventListener("keydown", (event) => {
